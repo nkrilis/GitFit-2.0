f1154d0fa8c26a0ffa52ce381019cb03b5236367
Nicholas Krilis <n.krilis@gmail.com>
2022-04-02 19:04:20 -0400

    Trying to deploy

--- server/server.js
+++ server/server.js
@@ -41,3 +41,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
 // Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
+
+// gitfit-admin
+// GitFitServer
