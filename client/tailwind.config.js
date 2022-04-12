35804491c82848c8b7a7acbc8c5dc640de287173
Mark Alfano <mpalfano4@gmail.com>
2022-04-12 16:14:19 -0400

    added custom shadow

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,24 +5,28 @@ module.exports = {
       black: "#1f1f1f",
       purple: {
         100: "#a545cc",
-        200: "#cb6ce6"
+        200: "#cb6ce6",
       },
       white: "#ffffff",
       gray: "#a1a1aa",
       blue: {
-        100: '#ebf8ff',
-        200: '#bee3f8',
-        300: '#90cdf4',
-        400: '#63b3ed',
-        500: '#4299e1',
-        600: '#3182ce',
-        700: '#2b6cb0',
-        800: '#2c5282',
-        900: '#2a4365',
+        100: "#ebf8ff",
+        200: "#bee3f8",
+        300: "#90cdf4",
+        400: "#63b3ed",
+        500: "#4299e1",
+        600: "#3182ce",
+        700: "#2b6cb0",
+        800: "#2c5282",
+        900: "#2a4365",
       },
-      
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        header:
+          "-5px -5px 25px -1px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
+      },
+    },
   },
   plugins: [],
 };
