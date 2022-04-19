f75af7c16499cd6989be7df92a0c1b8047b0432c
Mark Alfano <mpalfano4@gmail.com>
2022-04-18 23:17:14 -0400

    set up formData to grab inputs

--- client/src/pages/WorkoutUpdate.js
+++ client/src/pages/WorkoutUpdate.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { GET_EXERCISES, GET_WORKOUT_PLAN } from "../utils/queries";
 import { UPDATE_WORKOUT_PLAN } from "../utils/mutations";
 import { useQuery, useMutation } from "@apollo/client";
@@ -10,7 +10,7 @@ const WorkoutUpdate = () => {
   const exerciseList = dataExercise?.getExercises || [];
 
   const { loading, data: dataWorkoutPlan } = useQuery(GET_WORKOUT_PLAN, {
-    variables: { id: "test" },
+    variables: { id: "2" },
     fetchPolicy: "no-cache",
   });
   const workoutPlan = dataWorkoutPlan?.getWorkoutPlan || [];
@@ -19,8 +19,13 @@ const WorkoutUpdate = () => {
 
   const [updateWorkoutPlan] = useMutation(UPDATE_WORKOUT_PLAN);
 
+  const [weeks, setWeeks] = useState([]);
+  const [days, setDays] = useState([1]);
+
+  const exerciseForm = useRef(null);
+
   // function to create workout plan in database
-  const testUpdate = async () => {
+  const planUpdate = async () => {
     let planVal = [
       ...workoutPlan.plan[0].weeks[0].days.map((day) => {
         if (day.dayOfWeek === "Monday") {
@@ -67,7 +72,7 @@ const WorkoutUpdate = () => {
               {
                 exerciseId: "1",
                 userSets: 4,
-                userReps: 5,
+                userReps: 125,
               },
               {
                 exerciseId: "1",
@@ -158,7 +163,7 @@ const WorkoutUpdate = () => {
 
     await updateWorkoutPlan({
       variables: {
-        id: "test",
+        id: "2",
         title: workoutPlan.title,
         description: workoutPlan.description,
         type: workoutPlan.type,
@@ -177,8 +182,12 @@ const WorkoutUpdate = () => {
     });
   };
 
-  const [weeks, setWeeks] = useState([]);
-  const [days, setDays] = useState([1]);
+  const formSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(exerciseForm.current); //////////////////////////////////////////////////////////////////////////////////
+
+    for (const [y, z] of data) console.log(y, z);
+  };
 
   // Create array to map for weeks and days in workout
   const workoutWeeks = () => {
@@ -209,7 +218,7 @@ const WorkoutUpdate = () => {
 
   useEffect(() => {
     workoutWeeks();
-  }, [workoutPlan]);
+  }, [workoutPlan.numOfWeeks]);
 
   return (
     <div>
@@ -225,21 +234,24 @@ const WorkoutUpdate = () => {
                 Add another day
               </button>
             </div>
-            {days.map((day) => {
-              return (
-                <div key={week + day}>
-                  <h1>Day: {day} </h1>
-                  <form className="bg-purple-100 shadow-md px-2 py-2">
+            <form
+              ref={exerciseForm}
+              onSubmit={formSubmit}
+              className="bg-purple-100 shadow-md px-2 py-2"
+            >
+              {days.map((day) => {
+                return (
+                  <div key={week + day}>
+                    <h1>Day: {day} </h1>
                     <div className="grid grid-cols-7 gap-2">
                       <select
                         className="col-span-2 form-select form-select-sm appearance-none block w-full px-2 text-md text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         aria-label="exercise"
                       >
-                        <option selected>"Open this select menu"</option>
                         {exerciseList.map((exercise) => {
                           return (
                             <option
-                              id="option"
+                              id="exerciseSel"
                               name={exercise.name}
                               key={exercise._id}
                               value={exercise.name}
@@ -250,14 +262,27 @@ const WorkoutUpdate = () => {
                         })}
                       </select>
 
-                      <input type="number" min="1" max="50" placeholder="1" />
+                      <input
+                        name="sets"
+                        type="number"
+                        min="1"
+                        max="50"
+                        defaultValue={1}
+                      />
 
-                      <input type="number" min="1" max="50" placeholder="1" />
+                      <input
+                        name="reps"
+                        type="number"
+                        min="1"
+                        max="50"
+                        defaultValue={1}
+                      />
 
                       <textarea
                         className="col-span-2"
+                        name="description"
                         type="text"
-                        placeholder=""
+                        defaultValue={""}
                       ></textarea>
                       <button
                         className="font-xl hover:text-purple-100 hover:cursor-pointer hover:font-bold"
@@ -267,11 +292,11 @@ const WorkoutUpdate = () => {
                         Delete
                       </button>
                     </div>
-                  </form>
-                </div>
-              );
-            })}
-
+                  </div>
+                );
+              })}
+              <input type="submit" />
+            </form>
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
@@ -284,7 +309,7 @@ const WorkoutUpdate = () => {
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         type="button"
-        onClick={testUpdate}
+        onClick={planUpdate}
       >
         Update test
       </button>
